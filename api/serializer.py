4bc69dc51a1fe1255ce2e23b85107aefbe7f2b70
Andre <andre.acero@cetysuniversidad.edu.mx>
2025-02-20 11:53:57 -0800

    Agregar los serializers

--- api/serializer.py
+++ api/serializer.py
@@ -1,2 +1,34 @@
-from rest_framework import serializer
-from models import Categoria, Ingrediente, Receta, RecetaIngrediente, Menu
+from rest_framework import serializers
+from .models import Categoria, Ingrediente, Receta, RecetaIngrediente, Menu
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+
+class IngredienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingrediente
+        fields = '__all__'
+
+
+class RecetaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Receta
+        fields = '__all__'
+
+
+class RecetaIngredienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecetaIngrediente
+        fields = '__all__'
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    recetas = RecetaSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Menu
+        fields = '__all__'
